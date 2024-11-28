f5fbbaf71e01f259fdf0a86ddf0d16d51bd86a9c
Assimzhan Yersinov <a.yersinov@ovtech.kz>
2024-11-28 19:36:02 +0500

    Декларирование и инициализация переменных изучена, а также добавлены заметки

--- main.go
+++ main.go
@@ -4,4 +4,28 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World!")
+
+	//Декларация и инициализация переменной
+
+	var height int = 163
+
+	fmt.Println("My height is:", height)
+
+	//В чем полустрогость типизации:
+
+	var count = 154
+	fmt.Println("Count of houses is = ", count)
+
+	//Декларация и инициализация переменных одного типа (множественный случай)
+
+	var First, Second int = 12, 41
+	fmt.Printf("В деревне %d кур и %d слонов\n", First, Second)
+
+	//Декларирование блока переменных
+	var (
+		nameOfcharacter   string = "Carl"
+		powerOfcharacter  string = "FireBall"
+		HealthOfCharacter int    = 86
+	)
+	fmt.Printf("Статус персонажа: \nИмя персонажа: %s \nОсновной навык персонажа: %s \n Очки жизни персонажа: %d \n", nameOfcharacter, powerOfcharacter, HealthOfCharacter)
 }
