f1d47ecbb23397b4f04ce8aab53a1aefb61509e6
a.yersinov <a.yersinov@ovtech.kz>
2024-12-20 11:22:24 +0500

    Обновил калькулятор (добавил более расширенный вывод результата вычислений и оптимизировал ввод данных пользователя)

--- python/project_1.py
+++ python/project_1.py
@@ -1,26 +1,25 @@
 while True:
     symbol = input("Введите операцию расчета (+,*,/ или -): ")
 
-    if symbol == '+':
+    if symbol in ['+', '*', '-', '/']:
         number_1 = int(input("Введите первое число: "))
         number_2 = int(input("Введите второе число: "))
-        result = number_1 + number_2
-        print(result)
-    elif symbol == '*':
-        number_1 = int(input("Введите первое число: "))
-        number_2 = int(input("Введите второе число: "))
-        result = number_1 * number_2
-        print(result)
-    elif symbol == '/':
-        number_1 = int(input("Введите первое число: "))
-        number_2 = int(input("Введите второе число: "))
-        result = number_1 / number_2
-        print(result)
-    elif symbol == '-':
-        number_1 = int(input("Введите первое число: "))
-        number_2 = int(input("Введите второе число: "))
-        result = number_1 - number_2
-        print(result)
+    
+        if symbol == '+':
+            result = number_1 + number_2
+            print(f'Результат вычислений: {number_1} + {number_2} = {result}')
+        elif symbol == '*':
+            result = number_1 * number_2
+            print(f'Результат вычислений: {number_1} * {number_2} = {result}')
+        elif symbol == '/':
+            if number_2 == 0:
+                print('На ноль делить нельзя!')
+                continue
+            result = number_1 / number_2
+            print(f'Результат вычислений: {number_1} / {number_2} = {result}')
+        elif symbol == '-':
+            result = number_1 - number_2
+            print(f'Результат вычислений: {number_1} - {number_2} = {result}')
     elif symbol == 'exit':
         print('Вы прекращаете работу в калькуляторе. Пока.')
         break
