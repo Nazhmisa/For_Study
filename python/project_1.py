b3db1289dc922e3639827a4c4f77b4ad4b5c85bf
a.yersinov <a.yersinov@ovtech.kz>
2024-12-20 11:04:00 +0500

    Калькулятор готов - Добавлен выход из калькулятора и цикличность работы

--- python/project_1.py
+++ python/project_1.py
@@ -1,16 +1,29 @@
-symbol = input("Введите операцию расчета (+,*,/ или -): ")
-number_1 = int(input("Введите первое число: "))
-number_2 = int(input("Введите второе число: "))
+while True:
+    symbol = input("Введите операцию расчета (+,*,/ или -): ")
 
-if symbol == '+':
-    result = number_1 + number_2
-    print(result)
-elif symbol == '*':
-    result = number_1 * number_2
-    print(result)
-elif symbol == '/':
-    result = number_1 / number_2
-    print(result)
-else:
-    result = number_1 - number_2
-    print(result)
+    if symbol == '+':
+        number_1 = int(input("Введите первое число: "))
+        number_2 = int(input("Введите второе число: "))
+        result = number_1 + number_2
+        print(result)
+    elif symbol == '*':
+        number_1 = int(input("Введите первое число: "))
+        number_2 = int(input("Введите второе число: "))
+        result = number_1 * number_2
+        print(result)
+    elif symbol == '/':
+        number_1 = int(input("Введите первое число: "))
+        number_2 = int(input("Введите второе число: "))
+        result = number_1 / number_2
+        print(result)
+    elif symbol == '-':
+        number_1 = int(input("Введите первое число: "))
+        number_2 = int(input("Введите второе число: "))
+        result = number_1 - number_2
+        print(result)
+    elif symbol == 'exit':
+        print('Вы прекращаете работу в калькуляторе. Пока.')
+        break
+    else:
+        print('Такой операции не предусмотрено, выберите другой знак из перечисленных')
+        continue
