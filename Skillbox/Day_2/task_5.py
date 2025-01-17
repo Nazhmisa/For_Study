dd7ae18b5ddecea755e3cd60f768b229c7ec5ccb
a.yersinov <a.yersinov@ovtech.kz>
2025-01-17 16:08:21 +0500

    Разобрна Задача 5 из СкиллБокса

--- Skillbox/Day_2/task_5.py
+++ Skillbox/Day_2/task_5.py
@@ -1,8 +1,11 @@
 
 nums = int(input("Введите число: "))
+new_num = 1
 
-
-for num in range(1, nums, 2):
-    print(num)
-    for j in range(nums):
-        print(end=" ")
+for num in range(nums):
+    space_count = nums - num - 1
+    print("  " * space_count, end="")
+    for j in range(num+1):
+        print(new_num, end="  ")
+        new_num += 2
+    print()
